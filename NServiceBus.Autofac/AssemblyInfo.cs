46773427cdf350587f083544f62970a39e7a84e3
Eric Swann <eric.swann@q2ebanking.com>
2015-08-04 10:51:56 -0500

    Updated to beta 5 compatibility

---
+++ NServiceBus.Autofac/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NServiceBus Container Autofac Implementation")]
+[assembly: AssemblyDescription("Implementation of container functionality on top of Autofac.")]
+[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
+[assembly: AssemblyProduct("NServiceBus")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("NServiceBus.Autofac.Test")]
+[assembly: AssemblyVersion("5.1.0")]
+[assembly: AssemblyFileVersion("5.1.0")]
